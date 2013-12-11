4b858da6f7ee41892e22b5a5404a1655bc887d8f
devongrove <s-dgrove@lwsd.org>
2013-12-11 08:42:49 -0800

    Implemented StringValue class
    Still needs some work

--- src/VisiCalc/StringValue.java
+++ src/VisiCalc/StringValue.java
@@ -1,10 +1,33 @@
 package VisiCalc;
 
 public class StringValue extends Value{
-
+	private String value;
+	
 	public StringValue(String valueString) {
 		super(valueString);
-		// TODO Auto-generated constructor stub
+		setValue();
+		//displayValue();
 	}
 
+	private void setValue()
+	{
+		value = getInputStr();
+	}
+	
+	/**
+	private String displayValue()
+	{
+		String noQuotesValue = value;
+		char a;
+		a = value.charAt(0);
+		noQuotesValue = a + "";
+		return noQuotesValue;
+	}
+	**/
+	
+	public String getStringValue()
+	{
+		return value;
+	}
+	
 }
