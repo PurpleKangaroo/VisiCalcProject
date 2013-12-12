4ab4213b2eace5f8aec792cc2e55f16f4ff493b9
devongrove <s-dgrove@lwsd.org>
2013-12-12 08:56:13 -0800

    Prep for Checkpoint 2
    Added stuff for Checkpoint 2

--- src/VisiCalc/SpreadsheetGen.java
+++ src/VisiCalc/SpreadsheetGen.java
@@ -1,10 +1,21 @@
 package VisiCalc;
+import java.util.Scanner;
 
 public class SpreadsheetGen {
 	//This is a temporary way of printing the spreadsheet while the VFrame is still being implemented.
+	//TODO does not work
 	public static void main(String[] args)
 	{
 		Spreadsheet test = new Spreadsheet();
 		test.printSpreadsheet();
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine();
+		while (!userInput.equalsIgnoreCase("quit"))
+		{
+			test.printSpreadsheet();
+			userInput = sc.nextLine();
+		}
+		sc.close();
+		System.out.println("You have decided to quit.");
 	}
 }
