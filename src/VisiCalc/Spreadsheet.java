bddd5cf28a465c167a4f450e362448cf667a1ff1
Holt Maki <holtmaki@gmail.com>
2013-12-12 21:13:58 -0800

    Hopefully fixed githubs fuck up

--- src/VisiCalc/Spreadsheet.java
+++ src/VisiCalc/Spreadsheet.java
@@ -5,57 +5,10 @@ package VisiCalc;
  * @author Devon Grove
  * @author Holt Maki
  *
- */
-/*
-public class Spreadsheet {
-	private Cell[][] cells;
-	
-	public Spreadsheet()
-	{
-		cells = new Cell[22][12];
-		
-		for (int i = 0; i < 22; i++) 
-		{
-			for (int j = 0; j < 12; j++) 
-			{
-				cells[i][j] = new Cell("0");
-			}
-			
-		}
-	}
-	
-	public void printSpreadsheet() 
-	{
-		for (int i = 0; i < 22; i++) 
-		{
-			for (int j = 0; j < 12; j++) 
-			{
-				System.out.print(cells[i][j] + "\t");
-			}
-			
-			System.out.println();
-		}
-	}
-	
-	public Cell getCell(int column, int row)
-	{
-		return cells[column][row];
-	}
-	
-	public String getCellValueString()
-	{
-		return "THIS METHOD NOT COMPLETE";
-		//TODO complete getCellValueString method
-	}
-	
-}
 */
+
 public class Spreadsheet {
-<<<<<<< HEAD
 	private String[][] cells = new String[12][22]; 
-=======
-	String[][] cells = new String[12][22]; 
->>>>>>> 1aaa8e516523e73a23d1148c2cda45b1b5240429
 	private static final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L'};
 	//TODO GET THIS THING TO RECOGNIZE A USER SETTING THE CELL VALUES BY TYPING A1 = blahblahblah or 34
 	
@@ -67,7 +20,6 @@ public class Spreadsheet {
 		}
 	}
 	
-<<<<<<< HEAD
 	private int getMaxLength()
 	{
 		int a = 0;
@@ -93,19 +45,19 @@ public class Spreadsheet {
 				}
 				System.out.print(a + " ");
 			}
-=======
+		}
+	}
+
 	public void printSpreadsheet() {
 		for (int i = 0; i < 12; i++) {
 			for (int j = 0; j < 22; j++) {
 				System.out.print(cells[i][j] + " ");
 			}
 			System.out.println();
->>>>>>> 1aaa8e516523e73a23d1148c2cda45b1b5240429
 		}
 	}
 	
 	public void changeSpreadsheetValue(int row, int column, String newValue) {
-<<<<<<< HEAD
 		cells[column][row] = newValue;
 	}
 	
@@ -117,9 +69,3 @@ public class Spreadsheet {
 
 	
 }
-=======
-		cells[row][column] = newValue;
-	}
-	
-}
->>>>>>> 1aaa8e516523e73a23d1148c2cda45b1b5240429
