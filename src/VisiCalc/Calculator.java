506705cf5d416d3283463778b7cc5fdac8555d3e
HoltMaki <holtmaki@gmail.com>
2013-12-19 08:31:13 -0800

    calculator class changes - restart

--- src/VisiCalc/Calculator.java
+++ src/VisiCalc/Calculator.java
@@ -1,100 +1,31 @@
 package VisiCalc;
 
 public class Calculator {
-	private Operator[] operators;
+	private String expression;
+	private float value;
 	
-	public Calculator()
+	public Calculator(String expression)
+	{
+		this.expression = expression;
+		Calculate();
+	}
+
+	private void Calculate()
 	{
 		
 	}
 	
-	public float calculate(String expression) throws ParenthesisException
+	private void format()
 	{
-		expression = format(expression);
-		for (int i = 0; i < 10; i++)
-		{	expression.replaceAll(i + "(", i + "*(");
-			expression.replaceAll(")" + i, ")*" + i);
-		}
-		while (!finished())
-		operators = Operator.findOperations(expression);
-		getParenthesis(expression);
-		
-		
+		//TODO: Devon
+		//Make this so that it will format the String expression
+		//EX: "1+2 + -3 *9-2 *6^4/2*-9" -------> "1 + 2 + -3 * 9 - 2 * 6^4 / 2 * -9"
 	}
 	
-	private String getParenthesis(String expression) throws ParenthesisException
+	public float getValue()
 	{
-		//TODO use recursion to keep finding parenthesis inside of parenthesis.
-		int openCounter = 0;
-		int closeCounter = 0;
-		int level = 0;
-		int index = 0;
-		for (int i = 0; i < expression.length(); i++)
-		{
-			if (expression.charAt(i) == '(')
-			{
-				//Create new parenthesis
-				openCounter++;
-			}
-			if(expression.charAt(i) == ')')
-			{
-				//Close the correct parenthesis
-				closeCounter++;
-			}
-			if ((openCounter - closeCounter)> level)
-			{
-				level = openCounter - closeCounter;
-				index = i;
-			}
-		}
-		
-		int endIndex = 0;
-		
-		for (int i = index; i < expression.length(); i++)//*
-		{
-			if (expression.charAt(i) == ')')
-			{
-				endIndex = i;
-			}
-		}
-		Calculator parenthisisCalc = new Calculator();
-		float a = parenthisisCalc.calculate(expression.substring(index + 1, endIndex));
-		expression.replace(expression.substring(index+1, endIndex), a + "");
-		
-		/*if (openCounter != closeCounter)
-		{
-			ParenthesisException parenthesis = new ParenthesisException();
-			throw parenthesis;
-		}*/
+		return value;
 	}
 	
-	private String format(String expression)
-	{
-		String[] operatorWS = {" *", "* ", "+ ", " +", " -", "- ", "/ ", " /", "^ ", " ^", "( ", ") ", " (", " )"};
-		boolean formated = false;
-		while(!formated)
-		{
-			formated = true;
-			for (int i = 0; i < operatorWS.length; i++)
-			{
-				if( operatorWS[i]
-			}
-		}
-	}
 	
-	/**
-	 * A method that can tell if an expression is finished being calculated.
-	 * @param expression - the expression that is being calculated
-	 * @return
-	 */
-	private boolean finsihed(String expression)
-	{
-		//TODO: DEVON
-		//Take negatives into account ex: expression = -4
-		// in this case the expression is calculatted yet still contains a minus.
-		//but if the expression is 6 - 2, then the expression is not complete
-	}
-		
-		
-		
 }
