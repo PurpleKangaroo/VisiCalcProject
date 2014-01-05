f434b67de2bede186477fa5a2a305cab504b8806
HoltMaki <holtmaki@gmail.com>
2014-01-04 21:42:33 -0800

    a few listeners added
    These listeners are not fully finnished and functional though

--- src/VisiCalc/Spreadsheet.java
+++ src/VisiCalc/Spreadsheet.java
@@ -13,6 +13,7 @@ import java.net.URISyntaxException;
 */
 //TODO NEEDS SERIOUS FIXING. RIGHT NOW IT STORES EVERYTHING AS STRING, NOT CELL
 public class Spreadsheet {
+	private String fileName;
 	private Cell[][] cells; 
 	private static final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L'};
 	
@@ -72,6 +73,7 @@ public class Spreadsheet {
 	
 	public void save(String filename) throws FileNotFoundException, URISyntaxException
 	{
+		fileName = filename;
 		File saveFile = new File((new PathFinder()).getVisiCalc_Path(filename+".txt"));
 		PrintWriter out = new PrintWriter(saveFile);
 		for (int i = 0; i < 12; i++) {
@@ -81,8 +83,21 @@ public class Spreadsheet {
 		}
 	}
 	
+	public void save() throws URISyntaxException, FileNotFoundException
+	{
+		File saveFile = new File((new PathFinder()).getVisiCalc_Path(fileName+".txt"));
+		PrintWriter out = new PrintWriter(saveFile);
+		for (int i = 0; i < 12; i++) {
+			for (int j = 0; j < 22; j++) {
+				out.println(cells[i][j].getValue().getInputStr());
+			}
+		}
+		//FIXME add a try catch for this elswhere in the source code in case of the spreadsheet not having a filename.
+	}
+	
 	public void load(String filename)
 	{
+		fileName = filename;
 		//FIXME fill
 	}
 	
